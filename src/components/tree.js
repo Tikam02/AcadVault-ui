e2880252b3a01bd371613aea7f39707d604bb88d
Ishakikani9117 <ishakikani999@gmail.com>
2020-10-11 14:18:48 +0530

    update

--- src/components/tree.js
+++ src/components/tree.js
@@ -8,6 +8,35 @@ const getLabel = function (str) {
   return str.substring(str.lastIndexOf('/')+1);
 }
 
+const treeify = function (files) {
+  files = files.reduce(function(tree, f) {
+    const dir = path.dirname(f.path)
+
+    if (tree[dir]) {
+      tree[dir].children.push(f)
+    } else {
+      tree[dir] = { implied: true, children: [f] }
+    }
+
+    if (tree[f.path]) {
+      f.children = tree[f.path].children
+    } else {
+      f.children = []
+    }
+
+    return ((tree[f.path] = f), tree)
+  }, {})
+
+  return Object.keys(files).reduce(function(tree, f) {
+    if (files[f].implied) {
+      return tree.concat(files[f].children)
+    }
+
+    return tree
+  }, [])
+}
+
+
 function TreeStruture() {
     const [contentTree,setContentTree] = useState([]);
     useEffect(() => {
@@ -20,35 +49,7 @@ function TreeStruture() {
 
   const files = useMemo(() => contentTree.map(file => ({ ...file, label: getLabel(file.path) })), [contentTree])
 
-  function treeify(files) {
-    files = files.reduce(function(tree, f) {
-      const dir = path.dirname(f.path)
-  
-      if (tree[dir]) {
-        tree[dir].children.push(f)
-      } else {
-        tree[dir] = { implied: true, children: [f] }
-      }
-  
-      if (tree[f.path]) {
-        f.children = tree[f.path].children
-      } else {
-        f.children = []
-      }
-  
-      return ((tree[f.path] = f), tree)
-    }, {})
-  
-    return Object.keys(files).reduce(function(tree, f) {
-      if (files[f].implied) {
-        return tree.concat(files[f].children)
-      }
-  
-      return tree
-    }, [])
-  }
-
-  const treeStructure = treeify(files);
+  const treeStructure = useMemo(() => treeify(files), [files]);
 
   return(
     <Tree data={treeStructure}/>
